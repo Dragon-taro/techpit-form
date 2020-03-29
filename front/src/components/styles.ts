fe64f08daa2275b549e63b78877ca29ed9ddce2b
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-03-29 14:01:24 +0900

    環境構築

---
+++ front/src/components/styles.ts
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  title: {
+    margin: "32px 0 8px"
+  },
+  subTitle: {
+    margin: "8px 0 0"
+  },
+  textField: {
+    margin: "8px 0"
+  },
+  button: {
+    margin: "16px 0"
+  },
+  careerSpan: {
+    margin: "8px 0"
+  }
+});
+
+export default useStyles;
