458255665568f1bc78499542a145075906695d1f
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-03-31 01:01:04 +0900

    firebse移行

--- src/store/colleges/effects.ts
+++ src/store/colleges/effects.ts
@@ -4,7 +4,7 @@ import collegesActions from "./actions";
 export const searchColleges = (name: string) => async (dispach: Dispatch) => {
   dispach(collegesActions.searchCollege.started);
 
-  const url = `http://localhost:18001/colleges?name=${name}`;
+  const url = `https://us-central1-techpit-form-4e462.cloudfunctions.net/api/colleges?name=${name}`;
   const result = await fetch(url).then(res => res.json());
 
   dispach(
