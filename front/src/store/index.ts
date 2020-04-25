a8e7879096f03aee33e8f49c1e920f930227b4f4
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-04-26 00:53:25 +0900

    1-2

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -5,7 +5,9 @@ import { RootState } from "../domain/entity/rootState";
 const store = createStore(
   combineReducers<RootState>({
     profile: profileReducer
-  })
+  }),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
