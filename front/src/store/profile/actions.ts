007713d17d61d36a999d78554224a84bffa76a7f
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-04-30 23:08:16 +0900

    3-1

--- front/src/store/profile/actions.ts
+++ front/src/store/profile/actions.ts
@@ -1,13 +1,21 @@
 import actionCreatorFactory from "typescript-fsa";
 import { Profile } from "../../domain/entity/profile";
 import { Address } from "../../domain/entity/address";
+import { Career } from "../../domain/entity/career";
 
 const actionCreator = actionCreatorFactory();
 
 const profileActions = {
   setProfile: actionCreator<Partial<Profile>>("SET_PROFILE"),
   setAddress: actionCreator<Partial<Address>>("SET_ADDRESS"),
-  searchAddress: actionCreator.async<{}, Partial<Address>, {}>("SEARCH_ADDRESS")
+  searchAddress: actionCreator.async<{}, Partial<Address>, {}>(
+    "SEARCH_ADDRESS"
+  ),
+  setCareer: actionCreator<{ career: Partial<Career>; index: number }>(
+    "SET_CAREER"
+  ),
+  deleteCareer: actionCreator<number>("DELETE_CAREER"),
+  addCareer: actionCreator<{}>("ADD_CAREER")
 };
 
 export default profileActions;
--- front/src/store/profile/reducer.ts
+++ front/src/store/profile/reducer.ts
@@ -1,6 +1,7 @@
 import { reducerWithInitialState } from "typescript-fsa-reducers";
 import { Profile } from "../../domain/entity/profile";
 import profileActions from "./actions";
+import { Career } from "../../domain/entity/career";
 
 const init: Profile = {
   name: "",
@@ -12,7 +13,15 @@ const init: Profile = {
     prefecture: "",
     city: "",
     restAddress: ""
-  }
+  },
+  careers: []
+};
+
+const initCareer: Career = {
+  company: "",
+  position: "",
+  startAt: "",
+  endAt: ""
 };
 
 const profileReducer = reducerWithInitialState(init)
@@ -27,6 +36,20 @@ const profileReducer = reducerWithInitialState(init)
   .case(profileActions.searchAddress.done, (state, payload) => ({
     ...state,
     address: { ...state.address, ...payload.result }
+  }))
+  .case(profileActions.addCareer, state => ({
+    ...state,
+    careers: [...state.careers, initCareer]
+  }))
+  .case(profileActions.setCareer, (state, payload) => ({
+    ...state,
+    careers: state.careers.map((c, i) =>
+      i === payload.index ? { ...c, ...payload.career } : c
+    )
+  }))
+  .case(profileActions.deleteCareer, (state, payload) => ({
+    ...state,
+    careers: state.careers.filter((_, i) => i !== payload)
   }));
 
 export default profileReducer;
