5dc10dd7c817901e2cb03f04ad8054058877c962
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-05-10 17:40:19 +0900

    4-5

--- front/src/components/College.tsx
+++ front/src/components/College.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { TextField, Button } from "@material-ui/core";
+import {
+  TextField,
+  Button,
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem
+} from "@material-ui/core";
 
 import { RootState } from "../domain/entity/rootState";
 import collegesActions from "../store/colleges/actions";
 import { searchColleges } from "../store/colleges/effects";
+import { College as ICollege } from "../domain/entity/college";
+import profileActions from "../store/profile/actions";
 
 import useStyles from "./styles";
+import { PROFILE } from "../domain/services/profile";
 
 const College = () => {
   const dispatch = useDispatch();
   const colleges = useSelector((state: RootState) => state.colleges);
+  const profile = useSelector((state: RootState) => state.profile);
   const classes = useStyles();
+
+  const currentCollege = colleges.result.filter(
+    c => c.name === profile.college.name
+  )[0];
+  const currentFaculty = currentCollege?.faculty.filter(
+    f => f.name === profile.college.faculty
+  )[0];
 
   const handleChange = (name: string) => {
     dispatch(collegesActions.setSearchWord(name));
@@ -21,25 +40,108 @@ const College = () => {
     dispatch(searchColleges(colleges.search));
   };
 
+  const handleCollegeChange = (member: Partial<ICollege>) => {
+    dispatch(profileActions.setCollege(member));
+  };
+
+  const handleReset = () => {
+    handleCollegeChange({ name: "", faculty: "", department: "" });
+    dispatch(collegesActions.setSearchWord(""));
+    dispatch(collegesActions.searchCollege.done({ result: [], params: {} }));
+  };
+
   return (
     <>
-      <TextField
-        className={classes.formField}
-        fullWidth
-        label="大学名を検索"
-        value={colleges.search}
-        onChange={e => handleChange(e.target.value)}
-      />
-      <Button
-        fullWidth
-        variant="outlined"
-        color="primary"
-        className={classes.button}
-        onClick={handleSearch}
-        disabled={!colleges.search}
-      >
-        検索
-      </Button>
+      {!profile.college.name && (
+        <>
+          <TextField
+            className={classes.formField}
+            fullWidth
+            label="大学名を検索"
+            value={colleges.search}
+            onChange={e => handleChange(e.target.value)}
+          />
+          <Button
+            fullWidth
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+            onClick={handleSearch}
+            disabled={!colleges.search}
+          >
+            検索
+          </Button>
+          <Grid spacing={1} container>
+            {colleges.result.map(c => (
+              <Grid key={c.name} item>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => handleCollegeChange({ name: c.name })}
+                >
+                  {c.name}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+      {profile.college.name && (
+        <>
+          <TextField
+            className={classes.formField}
+            label={PROFILE.COLLEGE.NAME}
+            fullWidth
+            value={profile.college.name}
+            disabled
+          />
+          <FormControl fullWidth className={classes.formField}>
+            <InputLabel>{PROFILE.COLLEGE.FACULTY}</InputLabel>
+            <Select
+              value={profile.college.faculty}
+              onChange={e =>
+                handleCollegeChange({
+                  faculty: e.target.value as string,
+                  // 学科はリセットしないとwarnning
+                  department: ""
+                })
+              }
+            >
+              {currentCollege.faculty.map(f => (
+                <MenuItem key={f.name} value={f.name}>
+                  {f.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          {currentFaculty?.department.length > 0 && (
+            <FormControl fullWidth className={classes.formField}>
+              <InputLabel>{PROFILE.COLLEGE.DEPARTMENT}</InputLabel>
+              <Select
+                value={profile.college.department}
+                onChange={e =>
+                  handleCollegeChange({ department: e.target.value as string })
+                }
+              >
+                {currentFaculty.department.map(d => (
+                  <MenuItem key={d} value={d}>
+                    {d}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+          <Button
+            fullWidth
+            className={classes.button}
+            onClick={handleReset}
+            variant="outlined"
+            color="secondary"
+          >
+            学歴の入力情報をリセット
+          </Button>
+        </>
+      )}
     </>
   );
 };
