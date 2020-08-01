2a99e3d072e7f478d12beb4cbe4ecde234a60576
Dragon-taro <dragon.taro.lioil@gmail.com>
2020-08-01 13:19:10 +0900

    SPA化

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Profile from "./Profile";
 import Alert from "./Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Profile />
+      <Router>
+        <Switch>
+          <Route path="/edit">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
       <Alert />
     </>
   );
