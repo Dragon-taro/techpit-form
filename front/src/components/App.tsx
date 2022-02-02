d6cb14b0770ab6867eb3aa2ac0ce2780ab5d7cf7
kazu <kingkazuy2k@gmail.com>
2022-02-02 13:22:08 +0900

    Add:名前と自己紹介の追加

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Profile } from "./Profile";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Profile />
+    </>
+  );
 }
 
 export default App;
---
+++ front/src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+
+import { Basic } from "./Basic";
+import useStyles from "./styles";
+
+export const Profile = () => {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="sm">
+      <Typography
+        variant="h4"
+        component="h2"
+        className={classes.title}
+        color="primary"
+      >
+        基本情報
+      </Typography>
+      <Basic />
+    </Container>
+  );
+};
