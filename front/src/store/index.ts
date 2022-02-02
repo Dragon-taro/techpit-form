cc1fb6c9308dd58de939a86e6ffb3c2386c16515
kazu <kingkazuy2k@gmail.com>
2022-02-02 11:28:38 +0900

    Add:Redux追加

---
+++ front/src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from "redux";
+import { profileReducer } from "./profile/reducer";
+import { RootState } from "../domain/entity/rootState";
+
+export const store = createStore(
+  combineReducers<RootState>({
+    profile: profileReducer,
+  }),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
