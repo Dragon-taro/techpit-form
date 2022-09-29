4d6cb1641ea126efb7419508576551d5b8841dd1
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-29 22:28:46 +0900

    feat: Radioボタンのフォームを作成

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { TextField } from '@material-ui/core'
+import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@material-ui/core'
 
 import useStyles from './styles'
 
 import { RootState } from '../domain/entity/rootState'
 import { Profile } from '../domain/entity/profile'
 import { PROFILE } from '../domain/services/profile'
+import { Gender } from '../domain/entity/gender';
 import profileActions from '../store/profile/actions'
 
 const Basic = () => {
@@ -36,6 +37,25 @@ const Basic = () => {
                 onChange={e => handleChange({ description: e.target.value })}
                 label={PROFILE.DESCRIPTION}
             />
+            <FormControl className={classes.formField}>
+                <FormLabel>{PROFILE.GENDER}</FormLabel>
+                <RadioGroup
+                    value={profile.gender}
+                    onChange={e => handleChange({ gender: e.target.value as Gender})}
+                >
+                    <FormControlLabel 
+                        value='male'
+                        label="男性"
+                        control={<Radio color="primary" />}
+                    />
+                    <FormControlLabel 
+                        value='female'
+                        label="女性"
+                        control={<Radio color="primary" />}
+                    />
+                </RadioGroup>
+
+            </FormControl>
         </>
     )
 }
