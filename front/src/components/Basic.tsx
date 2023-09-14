e6031694e028d81ad68b5faecf3222baef2bf956
yitoigawa <yitoigawa@fab.pasona.tech>
2023-09-14 16:10:04 +0900

    feat: 文字列を扱うフォーム作成完了

--- front/src/components/Basic.tsx
+++ front/src/components/Basic.tsx
@@ -1,20 +1,39 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { TextField } from "@material-ui/core";
 
 import useStyles from "./styles";
+import { RootState } from "../domain/entity/rootState";
+import { Profile } from "../domain/entity/profile";
+import { PROFILE } from "../domain/services/profile";
+import profileActions from "../store/profile/actions";
 
 const Basic = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const profile = useSelector((state: RootState) => state.profile);
+
+    const handleChange = (member: Partial<Profile>) => {
+        dispatch(profileActions.setProfile(member));
+    };
 
     return (
         <>
-            <TextField fullWidth className={classes.formField} label="名前" />
+            <TextField 
+                fullWidth 
+                label={PROFILE.NAME}
+                className={classes.formField} 
+                value={profile.name}
+                onChange={e => handleChange({name: e.target.value})}
+            />
             <TextField
                 fullWidth
                 multiline
                 className={classes.formField}
                 rows={5}
-                label="自己紹介"
+                label={PROFILE.DESCRIPTION}
+                value={profile.description}
+                onChange={e => handleChange({description: e.target.value})}
             />
         </>
     );
---
+++ front/src/domain/services/profile.ts
@@ -0,0 +1,6 @@
+export const PROFILE = {
+    NAME: "名前",
+    DESCRIPTION: "自己紹介",
+    BIRTHDAY: "誕生日",
+    GENDER: "性別"
+};
